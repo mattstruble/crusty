6a056f0942ceaa4e7e53456421f923f12d971ca4
Matthew Struble <mstruble@localhost.localdomain>
2017-06-12 17:41:09 -0400

    Updated event structures and inheritance.

--- crusty/event.py
+++ crusty/event.py
@@ -10,5 +10,11 @@ from datetime import datetime
 
 class Event(object):
     
-    def __init__(self, time=None):
+    def __init__(self, etype=None, time=None):
+		self.etype = "EVENT" if etype is None else etype
         self.time = datetime.now() if time is None else time
+
+	def __repr__(self):
+		return 'Event({} {})'.format(self.etype, self.time)
+
+
--- crusty/input/keyboardevent.py
+++ crusty/input/keyboardevent.py
@@ -10,14 +10,16 @@ from crusty.event import Event
 
 class KeyboardEvent(Event):
 
-    KEY_DOWN = 0
-    KEY_UP = 1
+    KEY_DOWN = "KEY_DOWN"
+    KEY_UP = "KEY_UP"
     
     def __init__(self, etype, key, keycode, scancode, is_alt=False, time=None):
-        self.etype = etype
-        self.key = key
+		super(KeyboardEvent, self).__init__(etype, time)
+        
+		self.key = key
         self.keycode = keycode
         self.scan = scancode
         self.alt = is_alt
         
-        super(KeyboardEvent, self).__init__(time)
+	def __repr__(self):
+		return 'KeyboardEvent({} {} {})'.format(self.etype, self.key, self.time)
