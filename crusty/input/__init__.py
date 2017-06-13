9ee1cb369c83ffd92bd249580df9ca1188a5277a
mattstruble <mattstruble@outlook.com>
2017-06-12 23:27:57 -0400

    fixed imports

--- crusty/input/__init__.py
+++ crusty/input/__init__.py
@@ -1,5 +1,5 @@
 from _keyboardlistener import KeyboardListener as __KeyboardListener
-from crusty.eventhandler import EventHandler
+from eventhandler import EventHandler
 
 __kbListener = __KeyboardListener()
 __kbListener.addHandler(EventHandler.eventHandler)
