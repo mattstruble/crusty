6ea4a32a98559ebc40be97b6d541e14e69ebc056
mattstruble <mattstruble@outlook.com>
2017-06-12 23:25:41 -0400

    Fixed tabs/spacing hell on event and keyboard event

--- crusty/input/keyboardevent.py
+++ crusty/input/keyboardevent.py
@@ -6,20 +6,20 @@
 # 
 # Author: Matt Struble
 # Date: Sep. 11 2016
-from crusty.event import Event
+from event import Event
 
 class KeyboardEvent(Event):
 
-    KEY_DOWN = "KEY_DOWN"
-    KEY_UP = "KEY_UP"
+	KEY_DOWN = "KEY_DOWN"
+	KEY_UP = "KEY_UP"
     
-    def __init__(self, etype, key, keycode, scancode, is_alt=False, time=None):
+	def __init__(self, etype, key, keycode, scancode, is_alt=False, time=None):
 		super(KeyboardEvent, self).__init__(etype, time)
-        
+		
 		self.key = key
-        self.keycode = keycode
-        self.scan = scancode
-        self.alt = is_alt
-        
+		self.keycode = keycode
+		self.scan = scancode
+		self.alt = is_alt
+		
 	def __repr__(self):
 		return 'KeyboardEvent({} {} {})'.format(self.etype, self.key, self.time)
