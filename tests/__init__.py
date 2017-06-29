fd03ec4d8519caf798cfd6a8bea08307874164ff
Matthew Struble <mattstruble@outlook.com>
2017-06-29 16:50:09 -0400

    Stopped path from printing during tests on non-windows systems.

--- tests/__init__.py
+++ tests/__init__.py
@@ -6,4 +6,3 @@ if platform.system() == 'Windows':
     sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "\\..\\crusty")
 else:
     sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../crusty")
-    print sys.path
